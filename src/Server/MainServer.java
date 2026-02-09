54bc6c050293fdba93296a3158f3e3d173f1341c
Laura8720 <abbatilaura20@gmail.com>
2026-02-09 13:26:00 +0100

    First commit

--- src/Server/MainServer.java
+++ src/Server/MainServer.java
@@ -1,4 +1,21 @@
 package Server;
 
+import java.net.ServerSocket;
+import java.io.IOException;
+import java.net.Socket;
+
 public class MainServer {
+    public static void main(String[] args) {
+        System.out.println("SERVER: Inizio esecuzione!");
+        try {
+            ServerSocket server = new ServerSocket(3000);
+            System.out.println("SERVER: in attesa di richieste dei clienti");
+            Socket clientSocket = server.accept();
+            System.out.println("SERVER: il client si è connesso");
+        } catch (IOException e) {
+            System.err.println("La porta non è disponibilr," +
+                    "errore nella creazione del connection socket");
+        }
+
+    }
 }
