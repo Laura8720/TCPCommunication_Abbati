776cc6ace58faaff3fae384bb5b0d8db128fe7eb
achillebrac <abbatilaura20@gmail.com>
2026-02-18 11:40:18 +0100

    Third commit

--- src/MainClient/MainClient.java
+++ src/MainClient/MainClient.java
@@ -1,6 +1,8 @@
 package MainClient;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class MainClient {
@@ -8,6 +10,12 @@ public class MainClient {
         System.out.println("CLIENT: avvio del client!");
         try {
             Socket socket = new Socket("localhost", 3000);
+            System.out.println("CLIENT: il client si è connesso al server"); //3wayhandshake
+            OutputStream outputStream = socket.getOutputStream();
+            PrintWriter pw = new PrintWriter(outputStream);
+            pw.print("Ciao server!");
+            pw.flush();
+            System.out.println("CLIENT: il client ha inviato un messaggio");
         } catch (IOException e) {
             System.err.println("Errore nella connessione con il server");
         }
--- src/Server/MainServer.java
+++ src/Server/MainServer.java
@@ -1,5 +1,8 @@
 package Server;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.io.IOException;
 import java.net.Socket;
@@ -9,13 +12,24 @@ public class MainServer {
         System.out.println("SERVER: Inizio esecuzione!");
         try {
             ServerSocket server = new ServerSocket(3000);
-            System.out.println("SERVER: in attesa di richieste dei clienti");
+            System.out.println("SERVER: in attesa di richieste dei clienti"); //server in ascolto
             Socket clientSocket = server.accept();
             System.out.println("SERVER: il client si è connesso");
+            //comunicazione
+
+            //lettura
+            InputStream inputStream = clientSocket.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+            String messaggio = br.readLine();
+            System.out.println("SERVER: il client " + clientSocket
+                    + "ha scritto il messaggio " + messaggio);
+            //scrittura
+
+            //chiusura
             clientSocket.close(); //chiusura data socket
             server.close();
         } catch (IOException e) {
-            System.err.println("La porta non è disponibilr," +
+            System.err.println("La porta non è disponibile," +
                     "errore nella creazione del connection socket");
         }
 
